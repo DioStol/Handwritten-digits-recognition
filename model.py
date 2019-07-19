c3840d5ef7b53454070a309ed55feb34bc0030c0
DioStol <thalis@800px.com>
2019-07-19 16:21:12 +0300

    load data, remove noise, reshaping

--- model.py
+++ model.py
@@ -8,3 +8,15 @@ from keras import optimizers
 from keras.datasets import mnist
 from keras.utils import to_categorical
 import keras
+
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+
+
+_,X_train_th = cv2.threshold(X_train,127,255,cv2.THRESH_BINARY)
+_,X_test_th = cv2.threshold(X_test,127,255,cv2.THRESH_BINARY)
+
+
+X_train = X_train_th.reshape(-1,28,28,1)
+X_test = X_test_th.reshape(-1,28,28,1)
